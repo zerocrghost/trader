b1586151e3aa5cd14082c6a12ffedda65e0dce7f
BERRY <mhui0912@gmail.com>
2025-01-27 14:50:56 +0800

    add sniping list

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,19 @@
 const fs = require("fs")
 const { sellMint } = require("../bot/sell")
 
+const getSnipingAccount = async (req, res) => {
+    try {
+        let existingList = fs.readFileSync("./tx/snipingList.json", "utf-8")
+        existingList = JSON.parse(existingList)
+        const index = existingList.map(e => e.mint).indexOf(req.body.mint)
+        if (index < 0) {
+            return res.status(404).send({ msg: "Not found in history" })
+        }
+        return res.status(200).send({ data: existingList[index] })
+    } catch (err) {
+        res.status(501).send("server error")
+    }
+}
 const getBoughtList = async (req, res) => {
     try {
         let existingBoughtList = fs.readFileSync("./tx/boughtList.json", "utf-8")
@@ -35,6 +48,7 @@ const sell = async (req, res) => {
 }
 
 module.exports = {
+    getSnipingAccount,
     getBoughtList,
     sell,
     getTradeHis
