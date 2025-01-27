2b9565ff8202c7b289feefc68715d1fc4d8a2eca
BERRY <mhui0912@gmail.com>
2025-01-27 15:03:10 +0800

    added route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { startSubscribe, stopSubscribe, getSubscribeStatus } = require("../controllers/socketSubscribe");
-const { getBoughtList, sell, getTradeHis } = require("../controllers/controller");
+const { getBoughtList, sell, getTradeHis, getSnipingAccount } = require("../controllers/controller");
 const router = express.Router();
 
+router.get("/getSnipingAccount", getSnipingAccount);
 router.get("/getBoughtList", getBoughtList);
 router.get("/getTradeList", getTradeHis);
 router.get("/startSubscribe", startSubscribe)
